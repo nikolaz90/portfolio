417cab04a6840bb8dc1a2f02239ff24bacbe6e72
nikolaz.90 <nikolazstacey@hotmail.co.uk>
2022-09-28 14:14:11 +0200

    added new projects and added abit of box shadow to components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,15 +12,13 @@ function Navbar() {
   }
 
   return (
-    <nav className='nav'>
-      <div className='nav-content'>
+    <nav className='nav-content'>
         <h1><Link className='nav-logo' to='/'>Nikolaz.tech</Link></h1>
         <ul className='nav-links-container'>
           <li> <Link className={`nav-link ${isActive === 0 && 'active-nav'}`} to='/' onClick={()=>handleActivePage(0)}>Home</Link> </li>
           <li> <Link className={`nav-link ${isActive === 1 && 'active-nav'}`} to='/about' onClick={()=>handleActivePage(1)}>About</Link></li>
         </ul>
         <span className='theme-btn' onClick={setDarkTheme}>{isDark ? <small>Dark mode</small>:<small>Light mode</small>}</span>
-      </div>
     </nav>
   )
 }
--- src/projectsData.js
+++ src/projectsData.js
@@ -1,13 +1,27 @@
 
 const projects = [{
     id: 1, 
+    title: 'Regularly',
+    tech: 'ruby on rails', 
+    img: 'https://cdn.pixabay.com/photo/2017/03/18/04/20/app-2153173_1280.png',
+    description: "A simple web app to help people get into saving and for tracking these with an account",
+    link: 'https://mbs-regularly.fly.dev/',
+},{
+    id: 2, 
+    title: 'Landing page',
+    tech: 'html and css', 
+    img: 'https://cdn.pixabay.com/photo/2022/09/18/15/37/elephants-7463340_1280.jpg',
+    description: "Very simple landing page created for a timed challenge.",
+    link: 'https://monsieurbaguettes.com/landing_page/',
+},{
+    id: 3, 
     title: 'Pascale Art',
     tech: 'react.js', 
     img: 'https://images.ctfassets.net/fol21n7kmioh/2yVWWXH8Kd9pf0ytw9kMrz/0adc0212d45237d11615a3acb4ca2b43/img1.jpg',
     description: "An artist's personal/professional website to show and display art works using a headless CMS for all the content",
     link: 'https://pascalestaceyart.com',
 },{
-    id: 2, 
+    id: 4, 
     title: 'Savings tool',
     tech: 'react.js', 
     img: 'https://cdn.pixabay.com/photo/2016/08/15/17/38/piggy-bank-1595992_1280.jpg',
@@ -15,7 +29,7 @@ const projects = [{
     link: 'https://monsieurbaguettes.com/umbrelladay/',
 },
 {
-    id: 3, 
+    id: 5, 
     title: 'Cocktail recipes',
     tech: 'react.js', 
     img: 'https://cdn.pixabay.com/photo/2014/11/25/16/29/strawberry-545371_1280.jpg',
@@ -23,7 +37,7 @@ const projects = [{
     link: 'https://monsieurbaguettes.com/coqtails/',
 },
 {
-    id: 4, 
+    id: 6, 
     title: 'E-commerce',
     tech: 'JavaScript', 
     img: 'https://monsieurbaguettes.com/image-product-1.jpg',
@@ -31,7 +45,7 @@ const projects = [{
     link: 'https://monsieurbaguettes.com/sneakers.html',
 },
 {
-    id: 5, 
+    id: 7, 
     title: 'Spelling game',
     tech: 'JavaScript', 
     img: 'https://cdn.pixabay.com/photo/2015/08/13/01/00/keyboard-886462_1280.jpg',
@@ -39,7 +53,7 @@ const projects = [{
     link: 'https://monsieurbaguettes.com/Speel.html',
 },
 {
-    id: 6, 
+    id: 8, 
     title: 'Personal blog',
     tech: 'JavaScript', 
     img: 'https://monsieurbaguettes.com/DSC01587.JPG',
