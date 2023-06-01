72b3203957cc04993d332aa4aff056c15a605b59
nikolaz.90 <nikolazstacey@hotmail.co.uk>
2023-06-01 13:18:39 +0200

    added new profile pic

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import profileImage from '../images/Image1.png'
+import profileImage from '../images/redman.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import Stats from '../components/Stats'
