a7e980fbfb45166f7bbebcef8096e08f6284ccdd
nikolaz.90 <nikolazstacey@hotmail.co.uk>
2023-07-19 22:35:37 +0200

    finding the right project to display on single project page

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import { useGlobalContext } from '../context';
 
 function Project() {
     const {id} = useParams();
+    const {projectsData} = useGlobalContext();
+
+    const project = projectsData.find(project => project.id === Number(id));
+
+    console.log(project);
 
     return (
         <section>
-            {id}
+            <h2>{project.title}</h2>
+            <p>
+                tech used : {project.tech}
+            </p>
         </section>
     )
 }
