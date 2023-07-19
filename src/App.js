135febd3cd11c65cbe182c40d0d17102a5697cb0
nikolaz.90 <nikolazstacey@hotmail.co.uk>
2023-07-19 21:36:10 +0200

    added pages/project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 //pages
 import Home from './pages/Home'
 import About from './pages/About'
 import Articles from './pages/Articles'
 import Error from './pages/Error'
+import Project from './pages/Project'
+
 //components
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
@@ -18,6 +21,7 @@ function App() {
             <Route path='/' element={<Home/>} />
             <Route path='/about' element={<About/>}/>
             <Route path='/articles' element={<Articles/>}/>
+            <Route path='/project/:id' element={<Project/>} />
             <Route path='/*' element={<Error/>}/>
         </Routes>
         <Footer/>
